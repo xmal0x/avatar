a4b0db59b0420860ab475ef58f293c3b8d3431bf
kgalkin <malo2dis@gmail.com>
2025-09-18 19:51:26 +0200

    remove path

--- backend/app/core/paths.py
+++ backend/app/core/paths.py
@@ -1,4 +1,3 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parents[1]  # /app/app
-ROOT_DIR = BASE_DIR.parent                      # /app
+BASE_DIR = Path(__file__).resolve().parents[1]
